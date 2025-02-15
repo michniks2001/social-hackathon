474a3ce84f7af89b2b95a9dac7c4d51fe68ffb4c
Yang Li <6130276+yanglipmed@users.noreply.github.com>
2025-02-15 12:18:35 -0500

    yang v1

---
+++ sentiment_analyzer.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+
+# create a pipeline of sentiment analyzer
+sentiment_analyzer= pipeline("sentiment-analysis")
+
+# real-time sentiment analysis
+input="I hate this guy!"
+ouput=sentiment_analyzer(input)
+
+# postive or negative
+ouput[0]['label']
+
+# score
+ouput[0]['score']
